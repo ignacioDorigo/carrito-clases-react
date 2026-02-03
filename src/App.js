557ef91d741caf9512eed5b0a482a6a1b2cecd7a
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-03 19:18:29 -0300

    productos, productos, img public

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
 import "./box-model.css";
 import "./normalize.css";
+import Productos from "./components/Productos";
 
 class App extends Component {
+  state = {
+    productos: [
+      {
+        nombre: "Tomate",
+        precio: 1500,
+        urlImagen: "/imagenes/tomate.webp",
+      },
+      {
+        nombre: "Lechuga",
+        precio: 1500,
+        urlImagen: "/imagenes/lechuga.webp",
+      },
+      {
+        nombre: "Arbejas",
+        precio: 1500,
+        urlImagen: "/imagenes/arbejas.webp",
+      },
+    ],
+    carro: [],
+  };
   render() {
-    return <h2>Hola</h2>;
+    const agregarAlCarrito = () => {
+      console.log("Hola");
+    };
+
+    const sacarDelCarrito = () => {
+      console.log("Hola");
+    };
+    return (
+      <div>
+        <Productos
+          agregarAlCarrito={() => agregarAlCarrito}
+          sacarDelCarrito={() => sacarDelCarrito}
+          productos={this.state.productos}
+        ></Productos>
+      </div>
+    );
   }
 }
 export default App;
